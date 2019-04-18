1d48db791cb08246b7ad6053cc4c1fbdcd8f5325
NambiarSidharth <32272949+NambiarSidharth@users.noreply.github.com>
2019-04-18 17:00:30 +0530

    added level functionality

--- StateHandling/State.js
+++ StateHandling/State.js
@@ -1,4 +1,38 @@
-const state={
-    euphony:{}
-}
-module.exports = {state};
+const level = require("level")
+const db = level("%HOME%/Tincture/State")
+let state;
+
+db.get("state",(err,val)=>{
+    state=JSON.parse(val)
+    console.log(state)
+})
+// db.put("state",state,(err)=>{
+//     console.log("some error")
+// })
+// db.put("state/euphony",JSON.stringify(state),(err)=>{
+//     // Object.keys(val).forEach(obj=>{
+//     //     console.log()
+//     // })
+// })
+
+// db.get("euphony",(err,val)=>{
+//     console.log(val)
+// })
+// db.createReadStream()
+//   .on('data', function (data) {
+//     console.log(data.key, '=', data.value)
+//   })
+//   .on('error', function (err) {
+//     console.log('Oh my!', err)
+//   })
+//   .on('close', function () {
+//     console.log('Stream closed')
+//   })
+//   .on('end', function () {
+//     console.log('Stream ended')
+//   })
+
+const updateDB = ()=>{
+    db.put("state",JSON.stringify(state));
+    }
+module.exports = {state,updateDB};
--- StateHandling/TransactionHandlers/DataWriteHandler.js
+++ StateHandling/TransactionHandlers/DataWriteHandler.js
@@ -1,5 +1,22 @@
-let {state} = require("../State");
-
-const dataWrite = (brPath,data)=>{
-
+let {state,updateDB} = require("../State");
+let uuid4 = require("uuid/v4");
+const writeData=(store,branch,data)=>{
+    let  newKey=uuid4().toString()
+    state[store][branch][newKey]=data
+    updateDB()
+    return newKey;
 }
+
+const createBranch=(store,branch)=>{
+    state[store][branch]={}
+    updateDB()
+    return state[store]
+}
+
+const createSubStore = (store)=>{
+    state[store]={}
+    updateDB()
+    return state[store]
+}
+
+module.exports = {createBranch,createSubStore,writeData}
